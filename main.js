1298fbb1db0166c3e0322ad87cc7b031f2e3475f
Your Name <you@example.com>
2020-02-11 15:41:37 -0500

    updates

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ strurl('image-1','https://pbs.twimg.com/profile_images/903398169374973952/sq26Z9
 strurl('image-2','https://pathsensors.com/wp-content/uploads/potato-04.jpg')
 
 strurl('image-3','https://image.shutterstock.com/image-vector/pie-600w-324165815.jpg')   
+
+function strikeThrough(){
+    const list = document.querySelector('li');
+    list.style.textDecoration = 'line-through'
+
+
+}
+strikeThrough()
+
